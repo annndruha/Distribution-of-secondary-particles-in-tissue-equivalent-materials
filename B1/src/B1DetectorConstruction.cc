d05b14605a0887c31b1a8190893c8fb15b0a40bb
Andrey <andrey-crafty-fox@yandex.ru>
2021-09-20 23:18:16 +0300

    HW_1

--- B1/src/B1DetectorConstruction.cc
+++ B1/src/B1DetectorConstruction.cc
@@ -12,55 +12,53 @@
 #include "G4LogicalVolume.hh"
 #include "G4PVPlacement.hh"
 #include "G4SystemOfUnits.hh"
+#include "G4VisAttributes.hh"
 #include <math.h>
 
-
 B1DetectorConstruction::B1DetectorConstruction()
-: G4VUserDetectorConstruction(),
-  fScoringVolume(0)
-{ }
-
+    : G4VUserDetectorConstruction(),
+      fScoringVolume(0)
+{
+}
 
 B1DetectorConstruction::~B1DetectorConstruction()
-{ }
-
-
-G4VPhysicalVolume* B1DetectorConstruction::Construct()
 {
-  G4NistManager* nist = G4NistManager::Instance();
+}
+
+G4VPhysicalVolume *B1DetectorConstruction::Construct()
+{
+  G4NistManager *nist = G4NistManager::Instance();
 
   // Concrete cube
-  G4double Box_len = 20*cm;
-  G4Box* box = new G4Box("Box", Box_len, Box_len, Box_len);
-  G4Material* concrete = nist->FindOrBuildMaterial("G4_CONCRETE");
-  G4LogicalVolume* logicWorld_box = new G4LogicalVolume(box, concrete, "CONCRETE");
-
+  G4double box_len = 20 * cm;
+  G4Box *box = new G4Box("Box", box_len, box_len, box_len);
+  G4Material *concrete = nist->FindOrBuildMaterial("G4_CONCRETE");
+  G4LogicalVolume *logicWorld_box = new G4LogicalVolume(box, concrete, "CONCRETE");
+  G4VisAttributes *vis_concrete = new G4VisAttributes(G4Color(0.2, 0.2, 0.2));
+  logicWorld_box->SetVisAttributes(vis_concrete);
 
   // Graphite parallelepiped
-  G4double Para_len = 20*cm;
-  G4double pAlpha = M_PI/4;
-  G4double pTheta = 0;
-  G4double pPhi = 0;
-  G4Para* para = new G4Para("Parallelepiped", Para_len, Para_len, Para_len, pAlpha, pTheta, pPhi);
-  G4Material* graphite = nist->FindOrBuildMaterial("G4_GRAPHITE");
-  G4LogicalVolume* logicWorld_para = new G4LogicalVolume(para, graphite, "GRAPHITE");
+  G4Para *para = new G4Para("Parallelepiped", box_len, box_len, box_len, M_PI / 4, 0, 0);
+  G4Material *graphite = nist->FindOrBuildMaterial("G4_GRAPHITE");
+  G4LogicalVolume *logicWorld_para = new G4LogicalVolume(para, graphite, "GRAPHITE");
+  G4VisAttributes *vis_graphite = new G4VisAttributes(G4Color(0.4, 0.4, 0.4));
+  logicWorld_para->SetVisAttributes(vis_graphite);
 
   // Air world
-  G4double World_len = 100*cm;
-  G4Box* box_world = new G4Box("World", World_len, World_len, World_len);
-  G4Material* air = nist->FindOrBuildMaterial("G4_AIR");
-  G4LogicalVolume* logicWorld_mother = new G4LogicalVolume(box_world, air, "World");
+  G4Box *box_world = new G4Box("World", 100 * cm, 100 * cm, 100 * cm);
+  G4Material *air = nist->FindOrBuildMaterial("G4_AIR");
+  G4LogicalVolume *logicWorld_mother = new G4LogicalVolume(box_world, air, "World");
 
-
-  G4VPhysicalVolume* physWorld = 
-  new G4PVPlacement(0,
-                    G4ThreeVector(),
-                    logicWorld_mother,
-                    "World",
-                    0,
-                    false,
-                    0,
-                    true);
+  // Create world
+  G4VPhysicalVolume *physWorld =
+      new G4PVPlacement(0,
+                        G4ThreeVector(),
+                        logicWorld_mother,
+                        "World",
+                        0,
+                        false,
+                        0,
+                        true);
 
   new G4PVPlacement(0,
                     G4ThreeVector(),
@@ -72,7 +70,7 @@ G4VPhysicalVolume* B1DetectorConstruction::Construct()
                     true);
 
   new G4PVPlacement(0,
-                    G4ThreeVector(Box_len, 2*Box_len, 0), // Box located at parallelepiped surface
+                    G4ThreeVector(box_len, 2 * box_len, 0), // Box located at parallelepiped surface
                     logicWorld_box,
                     "Box",
                     logicWorld_mother,
@@ -80,5 +78,5 @@ G4VPhysicalVolume* B1DetectorConstruction::Construct()
                     0,
                     true);
 
-  return physWorld;                    
+  return physWorld;
 }
