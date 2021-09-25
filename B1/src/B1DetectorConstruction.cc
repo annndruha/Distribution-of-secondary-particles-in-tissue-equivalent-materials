5dd8f4ea1a3f76189f1c073a274a9ebf344b28ff
Andrey <andrey-crafty-fox@yandex.ru>
2021-09-26 00:40:42 +0300

    Try1 to fix runBeam error

--- B1/src/B1DetectorConstruction.cc
+++ B1/src/B1DetectorConstruction.cc
@@ -5,15 +5,18 @@
 
 #include "G4RunManager.hh"
 #include "G4NistManager.hh"
-
-#include "G4Box.hh"
-#include "G4Para.hh"
-
+#include "G4VisAttributes.hh"
 #include "G4LogicalVolume.hh"
 #include "G4PVPlacement.hh"
 #include "G4SystemOfUnits.hh"
-#include "G4VisAttributes.hh"
-#include <math.h>
+
+#include "G4Box.hh"
+#include "G4Para.hh"
+#include "G4Orb.hh"
+#include "G4Sphere.hh"
+#include "G4Trd.hh"
+
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
 
 B1DetectorConstruction::B1DetectorConstruction()
     : G4VUserDetectorConstruction(),
@@ -21,62 +24,110 @@ B1DetectorConstruction::B1DetectorConstruction()
 {
 }
 
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
+
 B1DetectorConstruction::~B1DetectorConstruction()
 {
 }
 
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
+
 G4VPhysicalVolume *B1DetectorConstruction::Construct()
 {
+  // Get nist material manager
   G4NistManager *nist = G4NistManager::Instance();
 
+  // Envelope parameters
+  //
+  G4double env_sizeXY = 150 * cm, env_sizeZ = 150 * cm;
+  G4Material *env_mat = nist->FindOrBuildMaterial("G4_AIR");
+
+  // Option to switch on/off checking of volumes overlaps
+  //
+  G4bool checkOverlaps = true;
+
+  //
+  // World
+  //
+  G4double world_sizeXY = 1.2 * env_sizeXY;
+  G4double world_sizeZ = 1.2 * env_sizeZ;
+  G4Material *world_mat = nist->FindOrBuildMaterial("G4_AIR");
+
+  G4Box *solidWorld =
+      new G4Box("World",                                                    //its name
+                0.5 * world_sizeXY, 0.5 * world_sizeXY, 0.5 * world_sizeZ); //its size
+
+  G4LogicalVolume *logicWorld =
+      new G4LogicalVolume(solidWorld, //its solid
+                          world_mat,  //its material
+                          "World");   //its name
+
+  G4VPhysicalVolume *physWorld =
+      new G4PVPlacement(0,               //no rotation
+                        G4ThreeVector(), //at (0,0,0)
+                        logicWorld,      //its logical volume
+                        "World",         //its name
+                        0,               //its mother  volume
+                        false,           //no boolean operation
+                        0,               //copy number
+                        checkOverlaps);  //overlaps checking
+
+  //
+  // Envelope
+  //
+  G4Box *solidEnv =
+      new G4Box("Envelope",                         //its name
+                env_sizeXY, env_sizeXY, env_sizeZ); //its size
+
+  G4LogicalVolume *logicEnv =
+      new G4LogicalVolume(solidEnv,    //its solid
+                          env_mat,     //its material
+                          "Envelope"); //its name
+
+  new G4PVPlacement(0,               //no rotation
+                    G4ThreeVector(), //at (0,0,0)
+                    logicEnv,        //its logical volume
+                    "Envelope",      //its name
+                    logicWorld,      //its mother  volume
+                    false,           //no boolean operation
+                    0,               //copy number
+                    checkOverlaps);  //overlaps checking
+  //==============================================================================
+  //
   // Concrete cube
-  G4double box_len = 20 * cm;
+  //
+  G4double box_len = 5 * cm;
+  G4double offset = 10 * cm;
+
+  G4ThreeVector pos1 = G4ThreeVector(offset, offset, offset);
   G4Box *box = new G4Box("Box", box_len, box_len, box_len);
   G4Material *concrete = nist->FindOrBuildMaterial("G4_CONCRETE");
-  G4LogicalVolume *logicWorld_box = new G4LogicalVolume(box, concrete, "CONCRETE");
-  G4VisAttributes *vis_concrete = new G4VisAttributes(G4Color(0.2, 0.2, 0.2));
-  logicWorld_box->SetVisAttributes(vis_concrete);
-
-  // Graphite parallelepiped
-  G4Para *para = new G4Para("Parallelepiped", box_len, box_len, box_len, M_PI / 4, 0, 0);
-  G4Material *graphite = nist->FindOrBuildMaterial("G4_GRAPHITE");
-  G4LogicalVolume *logicWorld_para = new G4LogicalVolume(para, graphite, "GRAPHITE");
-  G4VisAttributes *vis_graphite = new G4VisAttributes(G4Color(0.4, 0.4, 0.4));
-  logicWorld_para->SetVisAttributes(vis_graphite);
-
-  // Air world
-  G4Box *box_world = new G4Box("World", 100 * cm, 100 * cm, 100 * cm);
-  G4Material *air = nist->FindOrBuildMaterial("G4_AIR");
-  G4LogicalVolume *logicWorld_mother = new G4LogicalVolume(box_world, air, "World");
-
-  // Create world
-  G4VPhysicalVolume *physWorld =
-      new G4PVPlacement(0,
-                        G4ThreeVector(),
-                        logicWorld_mother,
-                        "World",
-                        0,
-                        false,
-                        0,
-                        true);
-
+  G4LogicalVolume *logicWorld_box = new G4LogicalVolume(box, concrete, "Box");
   new G4PVPlacement(0,
-                    G4ThreeVector(),
-                    logicWorld_para,
-                    "Para",
-                    logicWorld_mother,
-                    false,
-                    0,
-                    true);
-
-  new G4PVPlacement(0,
-                    G4ThreeVector(box_len, 2 * box_len, 0), // Box located at parallelepiped surface
+                    pos1, // Box located at parallelepiped surface G4ThreeVector(box_len, 2 * box_len, 0)
                     logicWorld_box,
                     "Box",
-                    logicWorld_mother,
+                    logicEnv,
                     false,
                     0,
                     true);
 
+  //
+  // Graphite parallelepiped
+  //
+  G4ThreeVector pos2 = G4ThreeVector(offset + box_len, offset + 2 * box_len, offset);
+  G4Para *para = new G4Para("Parallelepiped", box_len, box_len, box_len, M_PI / 4, 0, 0);
+  G4Material *graphite = nist->FindOrBuildMaterial("G4_GRAPHITE");
+  G4LogicalVolume *logicWorld_para = new G4LogicalVolume(para, graphite, "Parallelepiped");
+  new G4PVPlacement(0,
+                    pos2,
+                    logicWorld_para,
+                    "Para",
+                    logicEnv,
+                    false,
+                    0,
+                    true);
+
+  //==============================================================================
   return physWorld;
 }
