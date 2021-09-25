328f9a888000ed501ee485114263b738faaf631f
Andrey <andrey-crafty-fox@yandex.ru>
2021-09-26 01:06:51 +0300

    Half work

--- B1/src/B1DetectorConstruction.cc
+++ B1/src/B1DetectorConstruction.cc
@@ -1,3 +1,29 @@
+//
+// ********************************************************************
+// * License and Disclaimer                                           *
+// *                                                                  *
+// * The  Geant4 software  is  copyright of the Copyright Holders  of *
+// * the Geant4 Collaboration.  It is provided  under  the terms  and *
+// * conditions of the Geant4 Software License,  included in the file *
+// * LICENSE and available at  http://cern.ch/geant4/license .  These *
+// * include a list of copyright holders.                             *
+// *                                                                  *
+// * Neither the authors of this software system, nor their employing *
+// * institutes,nor the agencies providing financial support for this *
+// * work  make  any representation or  warranty, express or implied, *
+// * regarding  this  software system or assume any liability for its *
+// * use.  Please see the license in the file  LICENSE  and URL above *
+// * for the full disclaimer and the limitation of liability.         *
+// *                                                                  *
+// * This  code  implementation is the result of  the  scientific and *
+// * technical work of the GEANT4 collaboration.                      *
+// * By using,  copying,  modifying or  distributing the software (or *
+// * any work based  on the software)  you  agree  to acknowledge its *
+// * use  in  resulting  scientific  publications,  and indicate your *
+// * acceptance of all terms of the Geant4 Software license.          *
+// ********************************************************************
+//
+//
 /// \file B1DetectorConstruction.cc
 /// \brief Implementation of the B1DetectorConstruction class
 
@@ -5,16 +31,15 @@
 
 #include "G4RunManager.hh"
 #include "G4NistManager.hh"
-#include "G4VisAttributes.hh"
+#include "G4Box.hh"
+#include "G4Cons.hh"
+#include "G4Orb.hh"
+#include "G4Para.hh"
+#include "G4Sphere.hh"
+#include "G4Trd.hh"
 #include "G4LogicalVolume.hh"
 #include "G4PVPlacement.hh"
 #include "G4SystemOfUnits.hh"
-
-#include "G4Box.hh"
-#include "G4Para.hh"
-#include "G4Orb.hh"
-#include "G4Sphere.hh"
-#include "G4Trd.hh"
 
 //....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
 
@@ -39,7 +64,7 @@ G4VPhysicalVolume *B1DetectorConstruction::Construct()
 
   // Envelope parameters
   //
-  G4double env_sizeXY = 150 * cm, env_sizeZ = 150 * cm;
+  G4double env_sizeXY = 20 * cm, env_sizeZ = 30 * cm;
   G4Material *env_mat = nist->FindOrBuildMaterial("G4_AIR");
 
   // Option to switch on/off checking of volumes overlaps
@@ -76,8 +101,8 @@ G4VPhysicalVolume *B1DetectorConstruction::Construct()
   // Envelope
   //
   G4Box *solidEnv =
-      new G4Box("Envelope",                         //its name
-                env_sizeXY, env_sizeXY, env_sizeZ); //its size
+      new G4Box("Envelope",                                           //its name
+                0.5 * env_sizeXY, 0.5 * env_sizeXY, 0.5 * env_sizeZ); //its size
 
   G4LogicalVolume *logicEnv =
       new G4LogicalVolume(solidEnv,    //its solid
@@ -92,42 +117,79 @@ G4VPhysicalVolume *B1DetectorConstruction::Construct()
                     false,           //no boolean operation
                     0,               //copy number
                     checkOverlaps);  //overlaps checking
-  //==============================================================================
-  //
-  // Concrete cube
-  //
-  G4double box_len = 5 * cm;
-  G4double offset = 10 * cm;
 
-  G4ThreeVector pos1 = G4ThreeVector(offset, offset, offset);
+  //
+  // Shape 1
+  //
+  /*   G4Material* shape1_mat = nist->FindOrBuildMaterial("G4_A-150_TISSUE");
+  G4ThreeVector pos1 = G4ThreeVector(0, 2*cm, -7*cm);
+        
+  // Conical section shape       
+  G4double shape1_rmina =  0.*cm, shape1_rmaxa = 2.*cm;
+  G4double shape1_rminb =  0.*cm, shape1_rmaxb = 4.*cm;
+  G4double shape1_hz = 3.*cm;
+  G4double shape1_phimin = 0.*deg, shape1_phimax = 360.*deg;
+  G4Cons* solidShape1 =    
+    new G4Cons("Shape1", 
+    shape1_rmina, shape1_rmaxa, shape1_rminb, shape1_rmaxb, shape1_hz,
+    shape1_phimin, shape1_phimax);
+                      
+  G4LogicalVolume* logicShape1 =                         
+    new G4LogicalVolume(solidShape1,         //its solid
+                        shape1_mat,          //its material
+                        "Shape1");           //its name
+               
+  new G4PVPlacement(0,                       //no rotation
+                    pos1,                    //at position
+                    logicShape1,             //its logical volume
+                    "Shape1",                //its name
+                    logicEnv,                //its mother  volume
+                    false,                   //no boolean operation
+                    0,                       //copy number
+                    checkOverlaps);          //overlaps checking */
+
+  //
+  // Shape 2
+  //
+
+  // Concrete cube
+  G4double box_len = 5 * cm;
+  G4ThreeVector pos1 = G4ThreeVector(0, -1 * cm, 7 * cm);
+
   G4Box *box = new G4Box("Box", box_len, box_len, box_len);
   G4Material *concrete = nist->FindOrBuildMaterial("G4_CONCRETE");
   G4LogicalVolume *logicWorld_box = new G4LogicalVolume(box, concrete, "Box");
-  new G4PVPlacement(0,
-                    pos1, // Box located at parallelepiped surface G4ThreeVector(box_len, 2 * box_len, 0)
-                    logicWorld_box,
-                    "Box",
-                    logicEnv,
-                    false,
-                    0,
-                    true);
+  new G4PVPlacement(0,              //no rotation
+                    pos1,           //at position
+                    logicWorld_box, //its logical volume
+                    "Box",          //its name
+                    logicEnv,       //its mother  volume
+                    false,          //no boolean operation
+                    0,              //copy number
+                    checkOverlaps); //overlaps checking
 
-  //
   // Graphite parallelepiped
-  //
-  G4ThreeVector pos2 = G4ThreeVector(offset + box_len, offset + 2 * box_len, offset);
+  G4ThreeVector pos2 = G4ThreeVector(box_len, -1 * cm + 2 * box_len, 7 * cm);
   G4Para *para = new G4Para("Parallelepiped", box_len, box_len, box_len, M_PI / 4, 0, 0);
   G4Material *graphite = nist->FindOrBuildMaterial("G4_GRAPHITE");
   G4LogicalVolume *logicWorld_para = new G4LogicalVolume(para, graphite, "Parallelepiped");
-  new G4PVPlacement(0,
-                    pos2,
-                    logicWorld_para,
-                    "Para",
-                    logicEnv,
-                    false,
-                    0,
-                    true);
 
-  //==============================================================================
+  new G4PVPlacement(0,                //no rotation
+                    pos2,             //at position
+                    logicWorld_para,  //its logical volume
+                    "Parallelepiped", //its name
+                    logicEnv,         //its mother  volume
+                    false,            //no boolean operation
+                    0,                //copy number
+                    checkOverlaps);   //overlaps checking
+  // Set Shape2 as scoring volume
+  //
+  fScoringVolume = logicWorld_box;
+
+  //
+  //always return the physical World
+  //
   return physWorld;
 }
+
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
