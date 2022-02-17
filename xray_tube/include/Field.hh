b419614a8a4ddd6399f34025efcdbda0c89f8d02
Andrey <andrey-crafty-fox@yandex.ru>
2022-02-17 12:28:17 +0300

    Add magnetic field?

---
+++ xray_tube/include/Field.hh
@@ -0,0 +1,20 @@
+#ifndef ExN04Field_H
+#define ExN04Field_H 1
+
+#include "G4MagneticField.hh"
+#include "globals.hh"
+
+class ExN04Field : public G4MagneticField {
+public:
+  ExN04Field();
+  ~ExN04Field();
+
+  virtual void GetFieldValue( const  double Point[3], double *Bfield ) const;
+
+private:
+  G4double fBz;
+  G4double frmax_sq;
+  G4double fzmax;
+};
+
+#endif
---
+++ xray_tube/src/Field.cc
@@ -0,0 +1,30 @@
+#include "G4SystemOfUnits.hh"
+#include "Field.hh"
+
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
+ExN04Field::ExN04Field()
+ : G4MagneticField()
+{
+  fBz = 30.0*tesla;
+  frmax_sq = sqr(1000.*cm);
+  fzmax = 1000.*cm;
+}
+
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
+ExN04Field::~ExN04Field()
+{
+}
+
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
+void ExN04Field::GetFieldValue(const double Point[3],double *Bfield) const
+{
+  Bfield[0] = 0.;
+  Bfield[2] = 0.;
+  
+  if ( std::abs(Point[1]) < fzmax &&
+      (sqr(Point[0])+sqr(Point[2])) < frmax_sq ) {
+    Bfield[1] = fBz;
+  } else {
+    Bfield[1] = 0.;
+  }
+}
